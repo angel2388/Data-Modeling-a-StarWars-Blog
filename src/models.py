c4d6bdeabee9e7b061834c58663916f31cf1e38f
angel2388 <rafa2388@gmail.com>
2022-11-10 18:14:11 +0000

    Data Modeling a StarWars Blog

--- src/models.py
+++ src/models.py
@@ -8,26 +8,51 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    Nombre = Column(String(250))
+    Apellidos = Column(String(250))
+    Email = Column(String(250))
+    Nick = Column(String(250))
+    Contraseña = Column(String(250))
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    Nombre = Column(String(250))
+    
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    Nombre = Column(String(250))
+   
+
+class Vehiculos(Base):
+    __tablename__ = 'vehiculos'
+    id = Column(Integer, primary_key=True)
+    Nombre = Column(String(250))
+    
+
 
     def to_dict(self):
         return {}
 
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('usuario.id'))
+    planeta_id = Column(Integer,ForeignKey('planetas'))
+    planetas = relationship(Planetas)
+    personaje_id = Column(Integer,ForeignKey('personajes'))
+    personajes = relationship(Personajes)
+    vehiculo_id = Column(Integer,ForeignKey('vehiculos'))
+    vehiculos = relationship(Vehiculos)
+    
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
